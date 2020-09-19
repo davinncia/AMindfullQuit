d99bdaecf1f60f6d83101b73da0880e9cb39b61f
davincia <mahelucas1@gmail.com>
2020-09-19 12:06:11 +0200

    In app purchase working.

--- app/src/main/java/com/davinciapp/amindfullquit/developer/DeveloperActivity.kt
+++ app/src/main/java/com/davinciapp/amindfullquit/developer/DeveloperActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -16,6 +15,7 @@ import com.davinciapp.amindfullquit.utils.bind
 class DeveloperActivity : AppCompatActivity() {
 
     private val donateCoffeeButton by bind<Button>(R.id.btn_donation_coffee_developer)
+    private val donateSandwichButton by bind<Button>(R.id.btn_donation_sandwich_developer)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,17 +24,31 @@ class DeveloperActivity : AppCompatActivity() {
         //VM
         val viewModel = ViewModelProvider(this, ViewModelFactory.getInstance(application))[DeveloperViewModel::class.java]
 
-        findViewById<TextView>(R.id.tv_price_donation_coffee_developer).text = "2€"
-        findViewById<TextView>(R.id.tv_price_donation_sandwich_developer).text = "5€"
-
-        donateCoffeeButton.setOnClickListener {
-            Toast.makeText(this, "DONATE !!!!!!", Toast.LENGTH_SHORT).show()
-            viewModel.donate()
+        viewModel.message.observe(this) {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
         }
+        //BILLING
+        viewModel.billingFlowEvent.observe(this, {
+            viewModel.getBillingClient()?.launchBillingFlow(this, viewModel.getFlowParams()!!)
+        })
+
+        viewModel.coffeePrice.observe(this) {
+            findViewById<TextView>(R.id.tv_price_donation_coffee_developer).text = it
+        }
+        viewModel.sandwichPrice.observe(this) {
+            findViewById<TextView>(R.id.tv_price_donation_sandwich_developer).text = it
+        }
+
+
+        donateCoffeeButton.setOnClickListener { viewModel.setCoffeeBilling() }
+        donateSandwichButton.setOnClickListener { viewModel.setSandwichBilling() }
+
     }
 
 
     companion object {
         fun newIntent(context: Context): Intent = Intent(context, DeveloperActivity::class.java)
     }
+
+
 }
--- app/src/main/java/com/davinciapp/amindfullquit/developer/DeveloperViewModel.kt
+++ app/src/main/java/com/davinciapp/amindfullquit/developer/DeveloperViewModel.kt
@@ -2,6 +2,7 @@ package com.davinciapp.amindfullquit.developer
 
 import android.app.Application
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.android.billingclient.api.*
@@ -12,11 +13,20 @@ import kotlinx.coroutines.withContext
 class DeveloperViewModel(private val application: Application) : ViewModel(), PurchasesUpdatedListener {
 
     private var billingClient: BillingClient? = null
+    private var flowParams: BillingFlowParams? = null
+    private var skuDetailsResult: SkuDetailsResult? = null
+
+    val message = MutableLiveData<String>()
+
+    val coffeePrice = MutableLiveData<String>()
+    val sandwichPrice = MutableLiveData<String>()
+
+    val billingFlowEvent = MutableLiveData<Boolean>()
 
     //------------------------------------------------------------------------------------------//
     //                                 G O O G L E   P A Y M E N T
     //------------------------------------------------------------------------------------------//
-    fun donate() {
+    init {
         setUpBillingClient()
     }
 
@@ -30,13 +40,14 @@ class DeveloperViewModel(private val application: Application) : ViewModel(), Pu
         billingClient?.startConnection(object : BillingClientStateListener {
             override fun onBillingSetupFinished(billingResult: BillingResult) {
                 if (billingResult.responseCode ==  BillingClient.BillingResponseCode.OK) {
-                    // The BillingClient is ready. You can query purchases here.
+                    // The BillingClient is ready
+                    Log.d("debuglog", "Billing service connected!")
                     viewModelScope.launch { querySkuDetails(billingClient!!) }
                 }
             }
             override fun onBillingServiceDisconnected() {
-                // Try to restart the connection on the next request to
-                // Google Play by calling the startConnection() method.
+                Log.d("debuglog", "Billing service has disconnected, reconnecting...")
+                billingClient?.startConnection(this)
             }
         })
     }
@@ -44,22 +55,60 @@ class DeveloperViewModel(private val application: Application) : ViewModel(), Pu
     private suspend fun querySkuDetails(billingClient: BillingClient) {
         val skuList = ArrayList<String>()
         skuList.add("coffee_donation")
+        skuList.add("sandwich_donation")
+
         val params = SkuDetailsParams.newBuilder()
         params.setSkusList(skuList).setType(BillingClient.SkuType.INAPP)
-        val skuDetailsResult = withContext(Dispatchers.IO) {
-            billingClient.querySkuDetails(params.build())
+
+        skuDetailsResult = billingClient.querySkuDetails(params.build())
+
+        //Communicate prices to view
+        skuDetailsResult?.skuDetailsList?.let {
+            coffeePrice.value = it[0].price
+            sandwichPrice.value = it[1].price
         }
-        // Process the result.
-        // Retrieve a value for "skuDetails" by calling querySkuDetailsAsync().
-        val flowParams = BillingFlowParams.newBuilder()
-            .setSkuDetails(skuDetailsResult.skuDetailsList!![0])
-            .build()
-        val responseCode = billingClient.launchBillingFlow(DeveloperActivity(), flowParams)
+
+    }
+
+    fun setCoffeeBilling() {
+        skuDetailsResult?.let {
+            flowParams = BillingFlowParams.newBuilder()
+                .setSkuDetails(it.skuDetailsList!![0])
+                .build()
+
+            launchBillingFLowEvent()
+        }
+    }
+
+    fun setSandwichBilling() {
+        skuDetailsResult?.let {
+            flowParams = BillingFlowParams.newBuilder()
+                .setSkuDetails(it.skuDetailsList!![1])
+                .build()
+
+            launchBillingFLowEvent()
+        }
+    }
+
+    override fun onPurchasesUpdated(billingResult: BillingResult, purchases: MutableList<Purchase>?) {
+        viewModelScope.launch {
+            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK && purchases != null) {
+                for (purchase in purchases) {
+                    handlePurchase(purchase, billingClient!!)
+                }
+
+            } else if (billingResult.responseCode == BillingClient.BillingResponseCode.USER_CANCELED) {
+                message.value = "Billing canceled"
+            } else {
+                message.value = billingResult.debugMessage
+            }
+        }
     }
 
     private suspend fun handlePurchase(purchase: Purchase, client: BillingClient) {
         if (purchase.purchaseState == Purchase.PurchaseState.PURCHASED) {
             // Grant entitlement to the user.
+            message.value = "You are the best !"
 
             // Acknowledge the purchase if it hasn't already been acknowledged.
             if (!purchase.isAcknowledged) {
@@ -72,23 +121,13 @@ class DeveloperViewModel(private val application: Application) : ViewModel(), Pu
         }
     }
 
-    override fun onPurchasesUpdated(billingResult: BillingResult, purchases: MutableList<Purchase>?) {
-        viewModelScope.launch {
-            if (billingResult.responseCode == BillingClient.BillingResponseCode.OK && purchases != null) {
-                for (purchase in purchases) {
-                    handlePurchase(purchase, billingClient!!)
-                }
-                Log.d("debuglog", "DONATED")
-            } else if (billingResult.responseCode == BillingClient.BillingResponseCode.USER_CANCELED) {
-                // Handle an error caused by a user cancelling the purchase flow.
-                Log.d("debuglog", "NOT DONATED...")
-
-            } else {
-                // Handle any other error codes.
-                Log.d("debuglog", "NOT DONATED...")
-            }
-        }
+    //EVENTS
+    private fun launchBillingFLowEvent() {
+        billingFlowEvent.apply { value = if (this.value == null) true else !this.value!! }
     }
 
+    //GETTERS
+    fun getBillingClient() = billingClient
+    fun getFlowParams() = flowParams
 
 }
